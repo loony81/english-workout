234d95483098a8e1239e74ee3c0d1b8eeafb69b3
Roman Nazarenko <r.nazarenko.81@gmail.com>
2020-09-29 20:27:45 +0300

    initial setup

---
+++ app/assets/scripts/app.js
@@ -0,0 +1,6 @@
+import 'onsenui/css/onsenui.css'
+import 'onsenui/css/onsen-css-components.css'
+import 'onsenui'
+import '../styles/styles.css'
+
+if(module.hot) module.hot.accept()
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+	entry: './app/assets/scripts/app.js',
+	output: {
+		filename: 'bundled.js',
+		path: path.resolve(__dirname, 'app')
+	},
+	devServer: {
+		before(app,server){
+			server._watch('./app/**/*.html')
+		},
+		contentBase: path.join(__dirname, 'app'),
+		hot: true,
+		port: 3000,
+		host: '0.0.0.0'
+	},
+	mode: 'development',
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader?url=false']
+			}
+		]
+	}
+};
