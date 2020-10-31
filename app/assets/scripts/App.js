b367adbe69c92b5b7eddae0b11457e872f6fad20
Roman Nazarenko <r.nazarenko.81@gmail.com>
2020-10-31 17:42:10 +0200

    delete onsenui and restructure the project setup

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,30 +1,19 @@
 // For a css automated workflow we need to bundle our css files into our main javascript file
-// import onsen related modules
-import 'onsenui/css/onsenui.css'
-import 'onsenui/css/onsen-css-components.css'
-import * as ons from 'onsenui'
-import * as Ons from 'react-onsenui'
 // my own styles
-import '../styles/styles.css'
+import './App.css'
 // import React related modules
 import React from 'react'
 import ReactDOM from 'react-dom'
-import MyAmazingComponent from './modules/MyAmazingComponent'
+import MyAmazingComponent from './components/MyAmazingComponent'
 
 // for hot module replacement
 if(module.hot) module.hot.accept() 
 
-function handleClick(){
-	    ons.notification.alert('Hello world!');
-}
 
 const App = () => {
 	return (
 		<div>
-			<Ons.Page>
-	            <Ons.Button onClick={handleClick}>Tap me!</Ons.Button>
-	            <MyAmazingComponent />
-	        </Ons.Page>
+			<MyAmazingComponent/>
 		</div>
 	)
 }
--- app/assets/scripts/components/MyAmazingComponent.js
+++ app/assets/scripts/components/MyAmazingComponent.js
@@ -5,7 +5,6 @@ function MyAmazingComponent(){
 		<div>
 			<h1>This is my amazing React component</h1>
 			<p>React is great, the sky is blue, grass is green</p>
-			<ons-button onclick="alert('Hello World!')">Click Me</ons-button>
 			<img src="assets/images/warmup1.png" />
 		</div>
 	)
