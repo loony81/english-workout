e8502c5f5525501a2161d5fea2f18745f618d173
Roman Nazarenko <r.nazarenko.81@gmail.com>
2021-11-11 20:07:06 +0200

    create admin middleware, it will be applied later to the routes that are responsible for managing data (grammar, proverbs, translation)

---
+++ middleware/admin.js
@@ -0,0 +1,9 @@
+//this middleware function ensures that certain operations, such as deleting data or accessing certain data
+// can only be performed by admins
+//it must be executed after the authorization middlware function auth.js,
+// otherwise we won't have access to req.user
+module.exports = function (req,res,next){
+	//even if you are authenticated but not an admin, you are not allowed to access this resourse
+	if(!req.user.isAdmin) return res.status(403).send('Access denied.')
+	next()
+}
